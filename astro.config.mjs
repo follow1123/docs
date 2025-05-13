871ee42775ab58e8d53b7c4240f572f3d41e3255
yf <--->
2025-05-13 19:10:19 +0800

    migrate: neovim文档迁移完成

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from "@astrojs/starlight";
 import starlightThemeFlexoki from "starlight-theme-flexoki";
 import starlightKbd from "starlight-kbd";
 import starlightSidebarTopics from "starlight-sidebar-topics";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -27,9 +26,36 @@ export default defineConfig({
         starlightSidebarTopics([
           {
             label: "Neovim",
-            link: "/neovim/motions",
-            icon: "open-book",
-            items: ["neovim/motions", "neovim/modes"],
+            link: "/neovim",
+            items: [
+              "neovim",
+              {
+                label: "基础",
+                autogenerate: { directory: "neovim/basics" },
+              },
+              {
+                label: "搜索和替换",
+                autogenerate: { directory: "neovim/search-replace" },
+              },
+              {
+                label: "编辑环境",
+                autogenerate: { directory: "neovim/editing-environment" },
+              },
+              {
+                label: "定位和跳转",
+                autogenerate: { directory: "neovim/positioning-jumping" },
+              },
+              {
+                label: "其他",
+                autogenerate: { directory: "neovim/others" },
+              },
+              {
+                label: "内置插件",
+                autogenerate: { directory: "neovim/builtin-plugins" },
+              },
+              "neovim/neovim-api",
+            ],
+          },
           },
         ]),
       ],
---
+++ src/content/docs/neovim/index.md
@@ -0,0 +1,5 @@
+---
+title: 说明
+---
+
+官网: [Neovim](https://neovim.io)
