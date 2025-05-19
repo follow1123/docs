43658455c70e8b1a414623c219ea168d6648eea7
yf <--->
2025-05-19 15:42:45 +0800

    refactor: 实现开发时客户端渲染mermaid，构建时使用mermaid svg文件功能
    之前实时生成mermaid svg 文件的功能导致开发时很卡
    实现一个客户端渲染mermaid组件，和一个静态mermaid组件
    实现开发时使用客户端渲染，构建时使用静态mermaid文件功能
    构建前需要手动执行GEN_MERMAID=true astro build命令生成svg文件

---
+++ src/libs/mermaid/generator.ts
@@ -0,0 +1,86 @@
+import path from "path";
+import fs from "fs";
+import crypto from "crypto";
+
+type SvgFile = { lightPath: string; darkPath: string };
+
+class MermaidSvgCleaner {
+  #cache: Map<string, Array<string>>;
+
+  constructor() {
+    this.#cache = new Map();
+  }
+
+  set(dir: string, svgName: string) {
+    if (this.#cache.has(dir)) {
+      const svgNames = this.#cache.get(dir) as Array<string>;
+      svgNames.push(svgName);
+    } else {
+      const svgNames = new Array();
+      svgNames.push(svgName);
+      this.#cache.set(dir, svgNames);
+    }
+  }
+
+  clean() {
+    this.#cache.forEach((svgNames, dir) => {
+      try {
+        const files = fs.readdirSync(dir, {});
+        const unusedFiles = files.filter(
+          (f) => !svgNames.includes(f.toString()),
+        );
+        if (unusedFiles.length > 0) {
+          unusedFiles.forEach((f) => {
+            const unusedSvg = path.join(dir, f.toString());
+            console.log(`\nremove unused mermaid svg: ${unusedSvg}`);
+            fs.rmSync(unusedSvg, { recursive: true });
+          });
+        }
+      } catch (e) {
+        console.error(`\ncannot clean dir: ${dir}, error: ${e}`);
+      }
+    });
+  }
+}
+
+const publicPath = "public";
+const prefixPath = "mermaid-svg";
+const cleaner = new MermaidSvgCleaner();
+
+const generate = async (filePath: string, code: string): Promise<SvgFile> => {
+  filePath = path.normalize(filePath);
+  const dir = path.join(publicPath, prefixPath, filePath.replaceAll("/", "_"));
+  const svgName = crypto.createHash("md5").update(code, "utf8").digest("hex");
+  cleaner.set(dir, svgName);
+  const svgDir = path.join(dir, svgName);
+  fs.mkdirSync(svgDir, { recursive: true });
+
+  const lightPath = path.join(svgDir, "light.svg");
+  const darkPath = path.join(svgDir, "dark.svg");
+
+  if (fs.existsSync(darkPath) && fs.existsSync(lightPath)) {
+    console.log(`\nno need to rebuild, file: ${filePath}-${svgName}`);
+  } else {
+    try {
+      const m = await import("./renderer");
+      const renderer = m.default;
+      const lightId = `mermaid-${svgName}-light`;
+      const lightSvgContent = await renderer.render(lightId, code, "default");
+      const darkId = `mermaid-${svgName}-dark`;
+      const darkSvgContent = await renderer.render(darkId, code, "dark");
+      fs.writeFileSync(lightPath, lightSvgContent);
+      console.log(`\ngenerate light svg file: ${lightPath}`);
+      fs.writeFileSync(darkPath, darkSvgContent);
+      console.log(`\ngenerate dark svg file: ${darkPath}`);
+    } catch (e) {
+      console.error(`render mermaid svg error: ${e}`);
+    }
+  }
+  return { lightPath, darkPath };
+};
+
+process.on("exit", () => {
+  cleaner.clean();
+});
+
+export default generate;
---
+++ src/libs/mermaid/renderer.ts
@@ -0,0 +1,62 @@
+import puppeteer from "puppeteer-core";
+import type { Browser, Page } from "puppeteer-core";
+import type { Mermaid } from "mermaid";
+const chromePath = import.meta.env.CHROME_PATH;
+
+class Renderer {
+  #browser: Browser | undefined;
+  #page: Page | undefined;
+  constructor() {}
+
+  async init() {
+    const browser = await puppeteer.launch({
+      executablePath: chromePath,
+      headless: true,
+    });
+    this.#browser = browser;
+
+    const page = await browser.newPage();
+    await page.addScriptTag({
+      path: new URL(import.meta.resolve("mermaid/dist/mermaid.min.js"))
+        .pathname,
+    });
+    this.#page = page;
+  }
+
+  async deinit() {
+    if (this.#browser) {
+      await this.#browser.close();
+    }
+  }
+
+  async render(
+    id: string,
+    code: string,
+    theme: "default" | "dark",
+  ): Promise<string> {
+    if (!this.#page) await this.init();
+    const page = this.#page as Page;
+    return page.evaluate(
+      (id, code, theme) => {
+        // @ts-ignore
+        const mmd = mermaid as Mermaid;
+        mmd.initialize({ startOnLoad: true, theme });
+        return new Promise<string>((resolve, reject) => {
+          mmd
+            .render(id, code)
+            .then((result) => {
+              resolve(result.svg);
+            })
+            .catch((e) => reject(e));
+        });
+      },
+      id,
+      code,
+      theme,
+    );
+  }
+}
+
+const renderer = new Renderer();
+
+export default renderer;
